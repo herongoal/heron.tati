1c1ef9767090ac5260d1b9d3d4d0f8d8cccd964e
heron.sense.goal <heron.sense.goal@heron.sense.com>
2019-06-15 08:13:11 +0800

    add

--- heron_channel.cc
+++ heron_channel.cc
@@ -8,109 +8,6 @@
 
 using namespace std;
 namespace heron{namespace tati{
-heron_synch_buffer*  heron_synch_buffer::create(uint capacity)
-{
-        /**
-         * At least 1 byte should be reserved used to keep the wpos 1 byte
-         * before the rpos in which case the data in the buffer is not 
-         * continuous(the buffer is filled with data before the wpos or after the rpos,
-         * and the data after the rpos should be consumed first) so as to be
-         * distinguished from the other case, in which the data in the buffer
-         * is continuous and the rpos <= wpos(rpos == wpos stands for empty buffer).
-         */
-
-        heron_synch_buffer*    pbuff = new (std::nothrow)heron_synch_buffer(capacity + 1);
-        if(pbuff != nullptr)
-        {
-                pbuff->m_buff = (unsigned char *)malloc(pbuff->m_capacity);
-                if(pbuff->m_buff == nullptr)
-                {
-                        delete  pbuff;
-                        return  nullptr;
-                }
-        }
-
-        return  pbuff;
-}
-
-bool    heron_synch_buffer::fetch(void *data, unsigned int len)
-{
-        unsigned int    wpos = m_wpos;
-        if(wpos < m_rpos)
-        {
-                if(len <= m_capacity - m_rpos + wpos)
-                {
-                        if(m_rpos + len <= m_capacity)
-                        {
-                                memcpy(data, m_buff + m_rpos, len);
-                        }
-                        else
-                        {
-                                memcpy(data, m_buff + m_rpos, m_capacity - m_rpos);
-                                memcpy((char *)data + m_capacity - m_rpos, m_buff, len - m_capacity + m_rpos);
-                        }
-
-                        m_rpos = (m_rpos + len) % m_capacity;
-                        return  true;
-                }
-                else
-                {
-                        return  false;
-                }
-        }
-        else
-        {
-                if(len <= wpos - m_rpos)
-                {
-                        memcpy(data, m_buff + m_rpos, len);
-                        m_rpos = (m_rpos + len) % m_capacity;
-                        return  true;
-                }
-                else
-                {
-                        return  false;
-                }
-        }
-}
-
-bool    heron_synch_buffer::append(const void* data, unsigned len)
-{
-        unsigned int rpos = m_rpos;
-        if(m_wpos < rpos)
-        {
-                if(m_wpos + len + 1 <= rpos)
-                {
-                        memcpy(m_buff + m_wpos, data, len);
-                        m_wpos = (m_wpos + len) % m_capacity;
-                        return  true;
-                }
-                else
-                {
-                        return  false;
-                }
-        }
-        else
-        {
-                if(len > m_capacity - (m_wpos - rpos) - 1)
-                {
-                        return  false;
-                }
-
-                if(len <= m_capacity - m_wpos)
-                {
-                        memcpy(m_buff + m_wpos, data, len);
-                        m_wpos = (m_wpos + len) % m_capacity;
-                }
-                else
-                {
-                        memcpy((char *)m_buff + m_wpos, data, m_capacity - m_wpos);
-                        memcpy((char *)m_buff, (char *)data + m_capacity - m_wpos, m_wpos + len - m_capacity);
-                        m_wpos = m_wpos + len - m_capacity;
-                }
-                return  true;
-        }
-}
-
 heron_synch_channel*	heron_synch_channel::create(heron_engine *engine, uint channel_id){
 	heron_synch_channel* channel = new heron_synch_channel();
 	if(socketpair(AF_UNIX, SOCK_DGRAM, 0, channel->m_socketpair) < 0)
@@ -154,18 +51,18 @@ heron_synch_channel*	heron_synch_channel::create(heron_engine *engine, uint chan
 				channel_id, channel->m_socketpair[0], errno, strerror(errno));
 	}
 
-	for(uint n = 0; n < sizeof(channel->m_synch_buffs)/sizeof(channel->m_synch_buffs[0]); ++n){
-		channel->m_synch_buffs[n] = heron_synch_buffer::create(buf_len);
+	for(uint n = 0; n < sizeof(channel->m_queue)/sizeof(channel->m_queue[0]); ++n){
+		channel->m_queue[n] = heron_queue::create(buf_len);
 	}
 
 	return  channel;
 }
 
-heron_channel_routine::heron_channel_routine(heron_synch_buffer *buff, int fd):heron_routine(0,fd),m_buff(buff)
+heron_channel_routine::heron_channel_routine(heron_queue *buff, int fd):heron_routine(0,fd),m_buff(buff)
 {
 }
 
-heron_channel_routine* heron_channel_routine::create(heron_synch_buffer *buff, int fd)
+heron_channel_routine* heron_channel_routine::create(heron_queue *buff, int fd)
 {
 	return	nullptr;
 }
--- heron_channel.h
+++ heron_channel.h
@@ -3,6 +3,7 @@
 
 
 #include "heron_define.h"
+#include "heron_queue.h"
 #include "heron_routine.h"
 #include <cstdlib>
 #include <atomic>
@@ -10,69 +11,6 @@
 
 using namespace std;
 namespace heron{namespace tati{
-class   heron_synch_buffer
-{
-public:
-        static  heron_synch_buffer*    create(uint capacity);
-
-        heron_synch_buffer&    operator=(const heron_synch_buffer &b) = delete;
-        heron_synch_buffer(const heron_synch_buffer &b) = delete;
-
-        ~heron_synch_buffer()
-        {
-                if(m_buff != nullptr)
-                {
-                        free(m_buff);
-                        m_buff = nullptr;
-                }
-        }
-
-        bool            fetch(void *data, unsigned int len);
-
-        bool            append(const void* data, unsigned len);
-
-        unsigned        unused_len() const
-        {
-                unsigned int    rpos = m_rpos;
-                unsigned int    wpos = m_wpos;
-
-                if(wpos < rpos)
-                {
-                        return  rpos - wpos - 1;
-                }
-                else
-                {
-                        return  m_capacity - (wpos - rpos) - 1;
-                }
-        }
-
-        unsigned        data_len() const
-        {
-                int     rpos = m_rpos;
-                int     wpos = m_wpos;
-
-                if(wpos < rpos)
-                {
-                        return  m_capacity - rpos + wpos;
-                }
-                else
-                {
-                        return  wpos - rpos;
-                }
-        }
-
-private:
-        heron_synch_buffer(unsigned capacity):m_capacity(capacity),
-                m_rpos(0), m_wpos(0), m_buff(nullptr)
-        {
-        }
-	friend	class	heron_factory;
-        const unsigned int              m_capacity;
-        std::atomic<unsigned int>       m_rpos;
-        std::atomic<unsigned int>       m_wpos;
-        unsigned char*                  m_buff;
-};//end of class   heron_synch_buffer
-
 class	heron_channel_routine;
 class	heron_engine;
 
@@ -81,14 +19,14 @@ public:
 	static	heron_synch_channel*	create(heron_engine *engine, uint channel_id);
 private:
 	heron_synch_channel(){}
-	heron_synch_buffer*	m_synch_buffs[2];
-	int			m_socketpair[2];
+	heron_queue*	m_queue[2];
+	int		m_socketpair[2];
 };
 
 class	heron_channel_routine:public heron_routine{
 public:
-	heron_channel_routine(heron_synch_buffer *buff, int fd);
-	static	heron_channel_routine*	create(heron_synch_buffer *buff, int fd);
+	heron_channel_routine(heron_queue *buff, int fd);
+	static	heron_channel_routine*	create(heron_queue *buff, int fd);
 
 	
         virtual sint    on_event(heron_event ev){
@@ -104,7 +42,7 @@ public:
                return false;
         }
 protected:
-	heron_synch_buffer*	m_buff;
+	heron_queue*	m_buff;
 };
 }}//namespace heron::tati
 
